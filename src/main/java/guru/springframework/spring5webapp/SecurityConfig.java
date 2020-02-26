e0fdaca97640d16f390495820f291e070d84ba59
Piojag <piojag@gmail.com>
2020-02-26 21:38:25 +0100

    Interface

---
+++ src/main/java/guru/springframework/spring5webapp/SecurityConfig.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring5webapp;
+
+//https://www.youtube.com/watch?v=1S1XydtDgo8
+//https://www.youtube.com/watch?v=sm-8qfMWEV8&list=PLqq-6Pq4lTTYTEooakHchTGglSvkZAjnE
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Każda klasa Spring'a wymaga @Configuration
+ */
+
+@Configuration
+public abstract class SecurityConfig  {
+}
